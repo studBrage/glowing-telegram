91b0dccfc2ca96e28e2b001b69dce15e07d3cfdb
Bragebby <brageher@stud.ntnu.no>
2022-02-23 10:21:08 +0100

    add functionality for recursively placing watchers in directory

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,23 +1,45 @@
-package main
+package monitor
 
 import (
 	"fmt"
+	"io/fs"
+	"path/filepath"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-func main() {
-	fmt.Println("Hello")
+var watcher *fsnotify.Watcher
 
-	watcher, err := fsnotify.NewWatcher()
+func init() {
+	var err error
+	watcher, err = fsnotify.NewWatcher()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func Watch(dir string, c chan fsnotify.Event) {
 
 	defer watcher.Close()
 
+	fmt.Println("Watching following directories")
+	fmt.Println("======================================")
+	err := filepath.WalkDir(dir, func(path string, info fs.DirEntry, err error) error {
+		if info.IsDir() {
+			pth := strings.Replace(path, "\\", "/", -1)
+			fmt.Println(pth)
+			return watcher.Add(pth)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("======================================")
+
 	done := make(chan bool)
-	eventChan := make(chan string)
 
 	go func() {
 		for {
@@ -25,15 +47,21 @@ func main() {
 			case event := <-watcher.Events:
 				// fmt.Printf("EVENT! %#v\n", event)
 				// fmt.Println(event.String())
-				eventChan <- event.String()
+				// fmt.Println(event.Op.String() == "RENAME")
+				c <- event
 			case err := <-watcher.Errors:
 				fmt.Println("ERROR", err.Error())
 			}
 		}
 	}()
-	if err := watcher.Add("../destFolder"); err != nil {
-		fmt.Println(err.Error())
-	}
 
 	<-done
+}
+
+func AddWatcher(path string) error {
+	return watcher.Add(path)
+}
+
+func RemoveWatcer(path string) error {
+	return watcher.Remove(path)
 }
