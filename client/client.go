f6ea55a870058b7be3f92bc4c89ff7fe4b03fb8b
Bragebby <brageher@stud.ntnu.no>
2022-02-25 13:00:06 +0100

    Add funcs for how to write msg

--- client/client.go
+++ client/client.go
@@ -72,8 +72,30 @@ func WriteString(conn *net.TCPConn, msg string) {
 	time.Sleep(100 * time.Millisecond)
 }
 
-func writeByte(conn *net.TCPConn, msg []byte) {
-	fmt.Println("Sending msg")
-	conn.Write(msg)
+func WriteInfo(c *net.TCPConn, msg []byte) {
+	c.Write([]byte("INFO"))
 	time.Sleep(100 * time.Millisecond)
+	c.Write(msg)
+	time.Sleep(100 * time.Millisecond)
+}
+
+func WriteBytesChunk(c *net.TCPConn, msg []byte, chunckSize int) {
+	fmt.Println("Sending msg")
+	prev := 0
+	till := len(msg) - chunckSize
+	for prev < till {
+		next := prev + chunckSize
+		// fmt.Println(msg[prev:next])
+		c.Write(msg[prev:next])
+		prev = next
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Println(msg[prev:])
+	c.Write(msg[prev:])
+}
+
+func WriteFull(c *net.TCPConn, infomsg []byte, datamsg []byte, chunkSize int) {
+	WriteInfo(c, infomsg)
+	time.Sleep(100 * time.Millisecond)
+	WriteBytesChunk(c, datamsg, chunkSize)
 }
